b777c936e525521e855de1668958589aea8e83f5
Hristo Georgiev <h.georgiev@hotmail.com>
2024-02-02 07:53:03 +0200

    Add config for typescript & react formatting and spaces

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -48,6 +48,16 @@ require("formatter").setup({
 				}
 			end,
 		},
+		typescriptreact = {
+			-- prettier
+			function()
+				return {
+					exe = "prettier",
+					args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--single-quote" },
+					stdin = true,
+				}
+			end,
+		},
 		javascript = {
 			-- prettier
 			function()
