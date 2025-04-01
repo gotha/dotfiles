7123f9b0ee9900ee67d4dc67509d38394b5190c4
gotha <h.georgiev@hotmail.com>
2025-04-01 17:24:02 +0300

    Disable firefox ML chat

--- firefox/user.js
+++ firefox/user.js
@@ -2,3 +2,4 @@ user_pref('extensions.pocket.enabled', false);
 user_pref('browser.translations.automaticallyPopup', false);
 user_pref('sidebar.revamp', true);
 user_pref('sidebar.verticalTabs', true);
+user_pref('browser.ml.chat.enabled', false);
