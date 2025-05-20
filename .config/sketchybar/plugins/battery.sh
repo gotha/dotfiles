44f622626d7bdb27993ebe72d4168f95d2c43ab1
gotha <h.georgiev@hotmail.com>
2025-05-20 16:51:52 +0300

    change sketchybar batter icons

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -7,6 +7,19 @@ CHARGING=$(pmset -g batt | grep 'AC Power')
 # Set the right icon based on charging state and percentage
 if [[ $CHARGING != "" ]]; then
   ICON="󰂄"
+  case ${PERCENTAGE} in
+    100) ICON="󱟢" ;;
+    9[0-9]) ICON="󰂂" ;;
+    8[0-9]) ICON="󰂀" ;;
+    7[0-9]) ICON="󰁿" ;;
+    6[0-9]) ICON="󰁿" ;;
+    5[0-9]) ICON="󰁾" ;;
+    4[0-9]) ICON="󰁽" ;;
+    3[0-9]) ICON="󰁼" ;;
+    2[0-9]) ICON="󰁻" ;;
+    1[0-9]) ICON="󰁺" ;;
+    *) ICON="󰁹"
+  esac
   ICON_COLOR="0xffa6e3a1" # Green when charging
 else
   case ${PERCENTAGE} in
@@ -15,10 +28,10 @@ else
     8[0-9]) ICON="" ;;
     7[0-9]) ICON="" ;;
     6[0-9]) ICON="" ;;
-    5[0-9]) ICON="" ;;
-    4[0-9]) ICON="" ;;
+    5[0-9]) ICON="" ;;
+    4[0-9]) ICON="" ;;
     3[0-9]) ICON="" ;;
-    2[0-9]) ICON="" ;;
+    2[0-9]) ICON="" ;;
     1[0-9]) ICON="" ;;
     *) ICON="󰁹"
   esac
