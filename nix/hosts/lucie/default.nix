33400e71461449a68f941613761e13e2fd39f48e
gotha <h.georgiev@hotmail.com>
2025-09-21 13:25:54 +0300

    Add ssh tunnel service to lucie

--- nix/hosts/lucie/default.nix
+++ nix/hosts/lucie/default.nix
@@ -1,9 +1,34 @@
-{ ... }: {
+{ pkgs, ... }:
+let
+  cfg = import ../../config/default.nix;
+  username = cfg.username;
+in {
 
   imports = [ ./hardware-configuration.nix ../../os/linux/efi.nix ];
 
   networking.hostName = "lucie";
 
   services = { xserver.videoDrivers = [ "nvidia" ]; };
+
+  systemd.services.hg-tunnel = {
+    enable = true;
+    description = "Setup a secure tunnel to hgeorgiev.com";
+    after = [ "network.target" ];
+    wants = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      User = username;
+      ExecStart =
+        "${pkgs.openssh}/bin/ssh -NT -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -R 2224:localhost:22 gotha@hgeorgiev.com";
+
+      RestartSec = 5;
+      Restart = "always";
+
+      Type = "simple";
+    };
+
+    environment = { HOME = "/home/${username}"; };
+  };
 
 }
