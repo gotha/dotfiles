4d4bfa1c0df71b7c26902a955d93eebeb601f261
gotha <h.georgiev@hotmail.com>
2025-09-28 10:27:28 +0300

    Add mcphub and avante to neovim

--- home-manager/nvim/lua/plugins/packer.lua
+++ home-manager/nvim/lua/plugins/packer.lua
@@ -67,7 +67,50 @@ return require("packer").startup(function(use)
 
 	use("nelsyeung/twig.vim")
 
-	use("augmentcode/augment.vim")
+	--use("augmentcode/augment.vim")
+
+	use({
+		"ravitemer/mcphub.nvim",
+		run = "npm install -g mcp-hub@latest",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("mcphub").setup({
+				-- optional: defaults are fine to start
+			})
+		end,
+	})
+
+	use({
+		"yetone/avante.nvim",
+		branch = "main",
+		--run = "make BUILD_FROM_SOURCE=true",
+		run = "make",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"MeanderingProgrammer/render-markdown.nvim",
+			"stevearc/dressing.nvim", -- for enhanced input UI
+			"folke/snacks.nvim", -- for modern input UI
+		},
+		config = function()
+			require("avante").setup({
+				system_prompt = function()
+					local hub = require("mcphub").get_hub_instance()
+					return hub and hub:get_active_servers_prompt() or ""
+				end,
+				custom_tools = function()
+					return { require("mcphub.extensions.avante").mcp_tool() }
+				end,
+				provider = "ollama",
+				providers = {
+					ollama = {
+						endpoint = "http://127.0.0.1:11434",
+						model = "qwen2.5-coder:7b", -- :7b or :14b or :32b
+					},
+				},
+			})
+		end,
+	})
 
 	use("ktklin/confluence-cloud-vim")
 
