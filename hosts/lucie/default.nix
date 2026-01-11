3dc19975d6816de6eae2975abf2d95caab10c9c0
gotha <h.georgiev@hotmail.com>
2026-01-11 20:00:32 +0200

    disable tempo and grafana mcp servers for lucie

--- hosts/lucie/default.nix
+++ hosts/lucie/default.nix
@@ -105,8 +105,15 @@ in {
   };
 
   home-manager.users.${username} = {
-    # Disable GitHub MCP server for lucie because it does not currently work for linux
-    programs.mcp.enableGithub = false;
+    programs = {
+      mcp = {
+        # Disable GitHub MCP server for lucie because it does not currently work for linux
+        enableGithub = false;
+        # tempo and grafana servers require vpn that is not supported by this config yet
+        enableTempo = false;
+        enableGrafana = false;
+      };
+    };
   };
 
 }
