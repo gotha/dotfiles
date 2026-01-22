5c6a6e12830360b8ef99b67fcdcec802dfc686cc
gotha <h.georgiev@hotmail.com>
2026-01-22 21:34:46 +0200

    set https redirect for dissona proxy on bastion

--- hosts/bastion/nginx.nix
+++ hosts/bastion/nginx.nix
@@ -38,9 +38,14 @@
           proxy_set_header Host $host;
           proxy_set_header X-Real-IP $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_set_header X-Forwarded-Proto https;
           proxy_set_header X-Forwarded-Host $host;
-          proxy_set_header X-Forwarded-Port $server_port;
+          proxy_set_header X-Forwarded-Port 443;
+
+          # WebSocket support
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection "upgrade";
 
           proxy_buffering off;
         '';
