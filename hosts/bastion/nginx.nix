1cbfdfd7c8663ecbd3e5af419cabd8dc65aa7831
gotha <h.georgiev@hotmail.com>
2026-01-22 20:38:42 +0200

    enable dissona proxy on bastion

--- hosts/bastion/nginx.nix
+++ hosts/bastion/nginx.nix
@@ -27,6 +27,25 @@
         '';
       };
     };
+
+    virtualHosts."dissona.hgeorgiev.com" = {
+      forceSSL = true;
+      enableACME = true;
+
+      locations."/" = {
+        proxyPass = "http://10.100.0.100:7679";
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_set_header X-Forwarded-Host $host;
+          proxy_set_header X-Forwarded-Port $server_port;
+
+          proxy_buffering off;
+        '';
+      };
+    };
   };
 
   # Configure ACME for Let's Encrypt SSL certificates
