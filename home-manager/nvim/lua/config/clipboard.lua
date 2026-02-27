e68b7ef8691980beacee3bd35cbf468683401709
gotha <h.georgiev@hotmail.com>
2026-02-27 14:25:49 +0200

    configure tmux, kitty and nvim to allow pasting over ssh

--- home-manager/nvim/lua/config/clipboard.lua
+++ home-manager/nvim/lua/config/clipboard.lua
@@ -1,7 +1,26 @@
-if vim.fn.has("macunix") then
-	vim.opt.clipboard = "unnamedplus"
+-- Use OSC 52 for clipboard (works over SSH)
+-- Neovim 0.10+ has built-in OSC 52 support
+
+if vim.fn.has('nvim-0.10') == 1 then
+	-- Use OSC 52 as clipboard provider (works over SSH)
+	vim.g.clipboard = {
+		name = 'OSC 52',
+		copy = {
+			['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+			['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+		},
+		paste = {
+			['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+			['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+		},
+	}
+else
+	-- Fallback for older Neovim
+	if vim.fn.has("macunix") == 1 then
+		vim.opt.clipboard = "unnamedplus"
+	elseif vim.fn.has("unix") == 1 then
+		vim.opt.clipboard = "unnamed,unnamedplus"
+	end
 end
 
-if vim.fn.has("unix") then
-	vim.opt.clipboard = "unnamed,unnamedplus"
-end
+vim.opt.clipboard:append("unnamedplus")
