024596c6d93abc5a967454988c61f7fa7afcd724
gotha <h.georgiev@hotmail.com>
2026-03-05 22:25:47 +0200

    add friendly names for some apps in sketchybar

--- home-manager/sketchybar/plugins/front_app.sh
+++ home-manager/sketchybar/plugins/front_app.sh
@@ -3,6 +3,14 @@
 # Get the current app name and icon
 FRONT_APP=$(osascript -e 'tell application "System Events" to set frontApp to name of first application process whose frontmost is true')
 
+# Map Nix-wrapped app names to friendly names
+case "$FRONT_APP" in
+  ".kitty-wrapped") FRONT_APP="Kitty" ;;
+  ".alacritty-wrapped") FRONT_APP="Alacritty" ;;
+  ".firefox-wrapped") FRONT_APP="Firefox" ;;
+  "firefox") FRONT_APP="Firefox" ;;
+esac
+
 # Check if the app name is available
 if [[ $FRONT_APP != "" ]]; then
   sketchybar --set $NAME label="$FRONT_APP"
