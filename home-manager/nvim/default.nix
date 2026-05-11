b8e78199d5a3ba1615a9170908450f60646dc9ac
gotha <h.georgiev@hotmail.com>
2026-05-11 18:28:40 +0300

    explicitly list tools required by neovim plugins

--- home-manager/nvim/default.nix
+++ home-manager/nvim/default.nix
@@ -1,10 +1,35 @@
 { pkgs, ... }:
 {
+  # Tools that already have their own home-manager modules are pulled in here
+  # so importing nvim is enough to get its plugin dependencies.
+  imports = [
+    ../nodejs
+    ../vale
+  ];
 
   home.packages = with pkgs; [
     neovim
-    # Required for lazy.nvim to work
+    # Required for lazy.nvim to clone plugins.
     git
+    lazygit
+
+    # Formatters used by conform.nvim (lua/plugins/conform.lua).
+    rustfmt
+    djlint
+    ktlint
+    phpPackages.php-cs-fixer
+    # stylua, gofumpt, ruff, nixfmt and clang-tools are still sourced from
+    # config/packages-user.nix until per-language modules exist.
+
+    # LSPs configured in lua/plugins/nvim-lspconfig.lua.
+    rust-analyzer
+    phpactor
+    terraform-ls
+    regols
+    gopls
+    ruff
+    pyright
+    nixd
   ];
 
   xdg.configFile = {
@@ -14,5 +39,4 @@
     "nvim/lua/misc".source = ./lua/misc;
     "nvim/lua/plugins".source = ./lua/plugins;
   };
-
 }
