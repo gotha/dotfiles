156501451f768af7f495f5db74f7f5f625b7474c
gotha <h.georgiev@hotmail.com>
2026-05-29 07:51:50 +0300

    disable atlassian and dissona mcp for mucie

--- hosts/mucie/default.nix
+++ hosts/mucie/default.nix
@@ -19,6 +19,8 @@ in
   # (Grafana, Tempo), unused on this host (Gcloud), or
   # that we prefer not to load by default (Playwright).
   home-manager.users.${username}.programs.mcp = {
+    enableDissona = false;
+    enableAtlassian = false;
     enableGcloud = false;
     enableGrafana = false;
     enablePlaywright = false;
